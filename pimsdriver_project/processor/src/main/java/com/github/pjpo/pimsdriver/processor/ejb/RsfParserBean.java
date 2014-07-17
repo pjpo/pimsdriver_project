11d38fd1091437ae58f32f8c20b02cdaa4759a05
jean philippe delabre <jeanphilippe.delabre@gmail.com>
2014-07-17 10:21:28 +0200

    Pb de déginition de module eclipse, reprise à partir du début

--- pimsdriver_project/processor/src/main/java/com/github/pjpo/pimsdriver/processor/ejb/RsfParserBean.java
+++ pimsdriver_project/processor/src/main/java/com/github/pjpo/pimsdriver/processor/ejb/RsfParserBean.java
@@ -1,31 +1,12 @@
 package com.github.pjpo.pimsdriver.processor.ejb;
 
-import java.io.IOException;
 import java.io.Writer;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.LinkedList;
-import java.util.concurrent.Future;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-import javax.ejb.AsyncResult;
 import javax.ejb.PostActivate;
 import javax.ejb.PrePassivate;
 import javax.ejb.Stateful;
-
-import com.github.aiderpmsi.pims.parser.utils.PimsParserFromWriter;
-import com.github.aiderpmsi.pims.parser.utils.SimpleParser;
-import com.github.aiderpmsi.pims.parser.utils.SimpleParserFactory;
-import com.github.pjpo.pimsdriver.processor.ErrorCatcher;
-import com.github.pjpo.pimsdriver.processor.RsfLineHandler;
-import com.github.pjpo.pimsdriver.processor.ejb.RsfParser.ObservableValue.ObservableListener;
 
 @Stateful
 public class RsfParserBean implements RsfParser {
@@ -45,16 +26,6 @@ public class RsfParserBean implements RsfParser {
 	
 	@PostActivate
 	public void construct() {
-		try {
-			rsfResult = Files.createTempFile("", "");
-			rsfFile =  Files.createTempFile("", "");
-		} catch (Throwable e) {
-			if (rsfResult != null)
-				e = ErrorCatcher.execute(() -> { Files.deleteIfExists(rsfResult); rsfResult = null; }, e);
-			if (rsfFile != null)
-				e = ErrorCatcher.execute(() -> { Files.deleteIfExists(rsfFile); rsfFile = null; }, e);
-			LOGGER.log(Level.SEVERE, "Unable to create temp file", e);
-		}
 	}
 	
 	@Override
@@ -65,26 +36,18 @@ public class RsfParserBean implements RsfParser {
 
 
 	@Override
-	public ObservableValue<Float> processRsf(ObservableListener<Float> observer) {
+	public String processRsf() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Result getResult() {
-		Result result = new Result(null, null, null, null);
-		return result;
+	public String getResult() {
+		return null;
 	}
 	
 	@PrePassivate
 	public void destroy() {
-		Throwable e = null;
-		if (rsfResult != null)
-			e = ErrorCatcher.execute(() -> { Files.deleteIfExists(rsfResult); rsfResult = null; }, e);
-		if (rsfFile != null)
-			e = ErrorCatcher.execute(() -> { Files.deleteIfExists(rsfFile); rsfFile = null; }, e);
-		if (e != null)
-			LOGGER.log(Level.SEVERE, "Unable to delete temp file", e);
 	}
 
 }
